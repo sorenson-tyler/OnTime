983ac64dbf8420745228492e01d8c0769d7d4caf
Tyler Sorenson <tsorenson@navexglobal.com>
2017-03-31 11:01:24 -0700

    Save token inside our allowed space in openshift

--- Api/googleCalendar.js
+++ Api/googleCalendar.js
@@ -33,8 +33,7 @@
     // If modifying these scopes, delete your previously saved credentials
     // at ~/.credentials/calendar-nodejs-quickstart.json
     var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-    var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-        process.env.USERPROFILE) + '/.credentials/';
+    var TOKEN_DIR = '/.credentials/';
     var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
 
     // Load client secrets from a local file.
